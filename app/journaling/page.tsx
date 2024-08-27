44ef3f27f01a15917b0a35d4bebefd3945d0a614
Confvsio <rofclarel@gmail.com>
2024-08-27 16:01:55 +0200

    Journaling Update

--- app/journaling/page.tsx
+++ app/journaling/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
-import { PlusIcon, CalendarIcon, BookOpenIcon, TagIcon, SearchIcon } from '@heroicons/react/24/outline'
+import { PlusIcon, CalendarIcon, BookOpenIcon, TagIcon, MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 
 type Entry = {
   id: string
@@ -98,7 +98,7 @@ export default function JournalingPage() {
               onChange={(e) => setSearchTerm(e.target.value)}
               className="w-full pl-10 pr-4 py-2 border rounded-md"
             />
-            <SearchIcon className="h-5 w-5 text-gray-400 absolute left-3 top-3" />
+            <MagnifyingGlassIcon className="h-5 w-5 text-gray-400 absolute left-3 top-3" />
           </div>
         </div>
         <div className="overflow-y-auto h-full">
